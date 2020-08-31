6d47c7e77ed2813345219a8ec9fd325b121d821d
Paul Boone <narcissus.pong@gmail.com>
2020-08-31 11:29:50 -0400

    starting sqrt function

---
+++ extramath/sqrt.py
@@ -0,0 +1,3 @@
+
+def sqrt(x):
+    return x ** 0.5
---
+++ tests/test_sqrt.py
@@ -0,0 +1,8 @@
+
+from pytest import approx
+
+import extramath
+from extramath.sqrt import sqrt
+
+def test_sqrt():
+    assert sqrt(4.0) == approx(2.0)
